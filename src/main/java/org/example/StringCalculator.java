508ee66c8a349998b4d86707d1f3acc3d8a787c9
Andrzej Prochyra <andrzej@prochyra.io>
2025-10-06 17:38:11 +0100

    refactor: simplify logic

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -4,24 +4,13 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty())
             return 0;
-        if (numbers.contains(",")) {
-            String[] splitNumbers = numbers.split("[,\n]");
 
-            int sum = 0;
-            for (String splitNumber : splitNumbers) {
-                sum += Integer.parseInt(splitNumber);
-            }
-            return sum;
-        }
-        if (numbers.contains("\n")) {
-            String[] splitNumbers = numbers.split("\n");
+        String[] splitNumbers = numbers.split("[,\n]");
 
-            int sum = 0;
-            for (String splitNumber : splitNumbers) {
-                sum += Integer.parseInt(splitNumber);
-            }
-            return sum;
+        int sum = 0;
+        for (String splitNumber : splitNumbers) {
+            sum += Integer.parseInt(splitNumber);
         }
-        return Integer.parseInt(numbers);
+        return sum;
     }
 }
